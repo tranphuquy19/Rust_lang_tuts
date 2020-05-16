7299940d9f4a97c57f6e92d6d7e081e3b4eb8dc9
tranphuquy19 <tranphuquy19@gmail.com>
2020-05-16 17:23:12 +0700

    done r2

--- r2/r2_project/src/main.rs
+++ r2/r2_project/src/main.rs
@@ -1,3 +1,18 @@
+static PI: f32 = 3.14;
+
+fn sum(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut n: u8 = 8; // chỉ có thể khai báo trong fn
+    println!("PI = {}", PI);
+    println!("n = {}", n);
+    n = 9;
+    println!("n = {}", n);
+
+    let str: String = "asudhad".to_string();
+    println!("{}", str);
+
+    println!("SUM a + b = {}", sum(123, 254));
 }
