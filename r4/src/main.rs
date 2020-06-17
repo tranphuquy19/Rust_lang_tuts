68fbfa11e7ca7e921b56a6f64bb73f84b49c2b6a
tranphuquy19 <tranphuquy19@gmail.com>
2020-06-18 00:51:53 +0700

    r5

--- r4/src/main.rs
+++ r4/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     if a > 0 && a <= 100 {
         println!("{}", a);
     }
+
+    // let numbers = 0..1000000;
+
+    // for i in numbers {
+    //     println!("{}", i);
+    // }
+
+    let tup = (1,2,3, "ahihi");
+
+    println!("{:?}", tup);
 }
