d2c747eb7204034ee5c729337a117736d8b3467b
tranphuquy19 <tranphuquy19@gmail.com>
2020-06-18 01:27:53 +0700

    .

--- r5/src/main.rs
+++ r5/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut x = 10;
+    // let x2 = &x;
+    // let x3 = &mut x;
+
+    // println!("x is {}", x);
+    // println!("x is {}", x2);
+    // println!("x is {}", x3);
+
+    // *x3 += 1;
+
+    // println!("x is {}", x);
+    // println!("x is {}", x2);
+    // println!("x is {}", x3);
+
+    let mut x = 0;
+
+    let x2 = &mut x;
+    *x2 += 1;
+
+    println!("{}", x);
 }
